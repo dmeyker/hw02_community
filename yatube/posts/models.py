65ac01978316dde405cf9b96f6eac4ff80570fec
Дарья Мейкер <imeyker@gmail.com>
2022-07-25 20:57:23 +0300

    v1.1

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth import get_user_model
 # Автоматически создаем таблицу для пользователя
 User = get_user_model()
 
+
 class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
@@ -27,5 +28,5 @@ class Post(models.Model):
         Group,
         blank=True,
         null=True,
-        on_delete=models.CASCADE,
+        on_delete=models.CASCADE
     )
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
-#Главная страница
+
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {
         'title': 'Последние обновления на сайте',
         'posts': posts,
     }
-    return render(request, 'posts/index.html', context) 
+    return render(request, 'posts/index.html', context)
 
-#Сообщество
+
 def group_posts(request, slug):
-    group = get_object_or_404(Group,slug=slug)
+    group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
         'group': group,
